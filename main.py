ca6cf044ee752e7bec3fbea131d9394291cb3fd0
Amogh K Umesh <99596317+Amogh1001@users.noreply.github.com>
2023-06-26 17:36:05 +0530

    Updated main.py
    Added Youtube and Twitter

--- main.py
+++ main.py
@@ -1,28 +1,134 @@
+import tweepy
 from typing import Final
-
-# pip install python-telegram-bot
+import requests
+import json
+from googleapiclient.discovery import build
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from tokens import TOKEN, BOT_USERNAME, api_key, consumer_key, consumer_secret, access_token, access_token_secret
+
+TOKEN: Final = TOKEN
+BOT_USERNAME: Final = BOT_USERNAME
+api_key = api_key
+consumer_key = consumer_key
+consumer_secret = consumer_secret
+access_token = access_token
+access_token_secret = access_token_secret
+
+video_id = ""
+
+params = {
+    'q': 'ryzen',
+    'key': api_key
+}
+
+def video_comments(video_id, api_key):
+    # List for storing comments and replies
+    comments_and_replies = []
+
+    # Creating YouTube resource object
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    try:
+        # Retrieve YouTube video comments
+        video_response = youtube.commentThreads().list(
+            part='snippet,replies',
+            videoId=video_id,
+            maxResults=100,  # Increase if needed
+            textFormat='plainText'
+        ).execute()
+
+        # Iterate through video comments
+        while video_response:
+            for item in video_response['items']:
+                # Extract comment
+                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+
+                # Count number of replies to the comment
+                reply_count = item['snippet']['totalReplyCount']
+
+                if reply_count > 0:
+                    # Iterate through all replies
+                    for reply in item['replies']['comments']:
+                        # Extract reply
+                        reply_text = reply['snippet']['textDisplay']
+                        # Append comment and reply as a tuple
+                        comments_and_replies.append((comment, reply_text))
+
+                else:
+                    # Append comment and an empty reply as a tuple
+                    comments_and_replies.append((comment, ''))
+
+            # Check if there are more pages of results
+            if 'nextPageToken' in video_response:
+                video_response = youtube.commentThreads().list(
+                    part='snippet,replies',
+                    videoId=video_id,
+                    maxResults=100,
+                    textFormat='plainText',
+                    pageToken=video_response['nextPageToken']
+                ).execute()
+            else:
+                break
+
+    except Exception as e:
+        print('An error occurred:', str(e))
+
+    return comments_and_replies
 
 print('Starting up bot...')
 
-TOKEN: Final = '5843319916:AAFBrnkvS0Tnm0yjFtZCITwXRNnFna7RjJ4'
-BOT_USERNAME: Final = '@apitelegramtestamoghbot'
-
-
 # Lets us use the /start command
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello there! I\'m a bot. What\'s up?')
+    await update.message.reply_text('Hello there! I\'m a bot. What\'s up?\n/help: for help')
 
 
 # Lets us use the /help command
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Try typing anything and I will do my best to respond!')
+    await update.message.reply_text('/start: starts the bot')
+    await update.message.reply_text('/help: displays commands')
+    await update.message.reply_text('/echo <type here>: echos the message and displays the video')
+    await update.message.reply_text('/search <type here>: searches in youtube and displays the results')
+    await update.message.reply_text('/tweet <type here>: tweets the message')
 
+#Lets us use the /echo command
+async def echo_callback(update, context):
+    user_says = " ".join(context.args)
+    video_id = user_says
+    await update.message.reply_text("You said: " + video_id)
+    await update.message.reply_text("https://www.youtube.com/watch?v="+video_id)
+    comments = video_comments(video_id, api_key)
+    for comment, reply in comments:
+        await update.message.reply_text('Comment: '+ comment)
+        await update.message.reply_text('Reply: '+ reply+"\n")
 
-# Lets us use the /custom command
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('This is a custom command, you can add whatever text you want here.')
+async def search(update, context):
+    user_says = " ".join(context.args)
+    params['q'] = user_says
+    r = requests.get('https://www.googleapis.com/youtube/v3/search?', params=params, headers={'Accept': 'application/json'})
+    links=[]
+    data = r.json()
+    if 'items' in data:
+        for item in data['items']:
+            if(item['id']['kind'] != 'youtube#video'):
+                # print(item['id']['channelId'])
+                links.append("https://www.youtube.com/channel/"+item['id']['channelId'])
+            else:
+                # print(item['id']['videoId'])
+                links.append("https://www.youtube.com/watch?v="+item['id']['videoId'])
+    for link in links:
+        await update.message.reply_text(link)
+
+async def tweet(update, context):
+    user_says = " ".join(context.args)
+    twitter_client = tweepy.Client(
+        consumer_key=consumer_key, consumer_secret=consumer_secret,
+        access_token=access_token, access_token_secret=access_token_secret
+    )
+    response = twitter_client.create_tweet(
+        text=user_says
+    )
+    await update.message.reply_text("Tweeted: "+user_says)
 
 
 def handle_response(text: str) -> str:
@@ -77,8 +183,9 @@ if __name__ == '__main__':
     # Commands
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
-    app.add_handler(CommandHandler('custom', custom_command))
-
+    app.add_handler(CommandHandler("echo", echo_callback))
+    app.add_handler(CommandHandler("search", search))
+    app.add_handler(CommandHandler("tweet", tweet))
     # Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
