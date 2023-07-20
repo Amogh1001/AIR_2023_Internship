4dad9976143dfb1d60c1a892e26906307a97747f
Amogh K Umesh <99596317+Amogh1001@users.noreply.github.com>
2023-07-20 15:38:55 +0530

    Update read.php
    Converted return format to JSON

--- project/net/read.php
+++ project/net/read.php
@@ -1,9 +1,25 @@
 <?php
+ini_set('session.gc_maxlifetime', 1800);
+session_start();
+
+// Check if the session has expired
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 30)) {
+    // Session expired, redirect to the login page or perform logout
+    session_unset(); // Clear all session variables
+    session_destroy(); // Destroy the session data
+    header("Location: index.php");
+    exit();
+}
+
+// Update last activity time on each request
+$_SESSION['LAST_ACTIVITY'] = time();
+
+// Other PHP code follows...
 include('functions.php');
 $username = $_GET['login'];
 $password = $_GET['pwd'];
 
-// Perform your auth    entication logic
+// Perform your authentication logic
 error_reporting(E_ALL);
 $servername = "localhost";
 $username = "root";
@@ -15,9 +31,9 @@ if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 if ($username === 'admin' && $password === '1111') {
-    read($mysqli, 1);
+    display($mysqli, 1);
     echo 'Login successful!';
 } else {
-    read($mysqli, 0);
+    echo json_encode(display($mysqli, 0));
 }
 ?>
