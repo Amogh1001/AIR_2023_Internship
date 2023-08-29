b8d046257c1a7f3a57870ff6329ea5a147b935d0
Akshat723 <143522627+Akshat723@users.noreply.github.com>
2023-08-29 21:23:11 +0530

    Update index.php
    Added documentation for clarity of code

--- project/index.php
+++ project/index.php
@@ -1,29 +1,47 @@
+<!-- This is an HTML form that allows the user to login. -->
 <html>
 
 <form method="get">
+    <!-- The `login` and `pwd` input fields are used to collect the user's login credentials. -->
     login: <input type="text" name="login"><br>
     pwd: <input type="text" name="pwd"><br>
+    <!-- The `submit` button submits the form data to the PHP script. -->
     <input type="submit" name="submit">
 </form>
 
+<!-- This is a link to the `create.php` script, which allows the user to create a new account. -->
 <form action="http://localhost/project/net/create.php" method="get" target="_blank">
+  <!-- The `submit` button opens the `create.php` script in a new tab. -->
   <input type="submit" value="Sign Up">
 </form>
 
 <?php
-    include('net/functions.php');
-    error_reporting(E_ALL);
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "amogh";
-    $mysqli = new mysqli($servername, $username, $password, $dbname);
-    if ($mysqli->connect_error) {
-        die("Connection failed: " . $mysqli->connect_error);
-    }
-    if(isset($_GET['submit'])) {
-        read($mysqli, 0);
-    }
+  // This PHP script includes the `functions.php` script, which contains the `read()` function.
+  include('net/functions.php');
+
+  // This line sets the error reporting level to E_ALL.
+  error_reporting(E_ALL);
+
+  // This line defines the MySQL database connection variables.
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "amogh";
+
+  // This line creates a new MySQLi object.
+  $mysqli = new mysqli($servername, $username, $password, $dbname);
+
+  // This line checks if the connection to the MySQL database was successful.
+  if ($mysqli->connect_error) {
+    // If the connection was not successful, an error message is displayed.
+    die("Connection failed: " . $mysqli->connect_error);
+  }
+
+  // This code is executed if the `submit` button is clicked.
+  if(isset($_GET['submit'])) {
+    // The `read()` function is called to read the employee records from the database.
+    read($mysqli, 0);
+  }
 ?>
 
 </html>
